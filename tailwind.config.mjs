63a96bb5c2595844fcbf15dcab061242e9e00aae
Francisco <javiermartincuenca@gmail.com>
2024-04-12 21:50:05 +0200

    Added custom fonts, colors and size

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,27 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#3e4143',
+          DEFAULT: '#1c1d1f',
+        },
+        udemy: {
+          light: '#a436f0',
+          DEFAULT: '#5624d0',
+          dark: '#401b9c',
+        },
+        accent: '#eceb98',
+      },
+      maxWidth: {
+        '8xl': '84rem',
+      },
+      fontFamily: {
+        heading: ['Noto Serif JP', 'serif'],
+        text: ['Roboto', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
